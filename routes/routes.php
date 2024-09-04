1ffcbeeffdf84906e3f39a9581d7149dd07ce63f
rustam.k <rusik.19101997@gmail.com>
2024-09-04 15:29:24 +0500

    ajax desc route

--- routes/routes.php
+++ routes/routes.php
@@ -6,11 +6,16 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Controllers\CitiesController;
 use App\Controllers\CountryController;
+use App\Controllers\GetDescriptionController;
 
 $router = new Router();
 
 $router->get('/', function(Request $request) {
     return (new CountryController)->index($request);
+});
+
+$router->get('/', function(Request $request) {
+    return (new GetDescriptionController)->getDescription($request);
 });
 
 $router->run();
